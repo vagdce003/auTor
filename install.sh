951fd105c496e6b34489186a61520018304a34ad
vagdce003 <66467535+vagdce003@users.noreply.github.com>
2020-06-05 10:00:26 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ cd torghost
 chmod +x install.sh
 yes | ./install.sh
 torghost -s
+wget -O anydesk.deb https://download.anydesk.com/linux/anydesk_5.5.6-1_amd64.deb?_ga=2.177290535.303793319.1591351136-604178921.1591351136 
+apt install ./anydesk.deb
